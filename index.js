18948f245c8c55cc0658b1d3ab751690854d2772
shaai <shaai.sattar@gmail.com>
2014-12-14 10:42:56 -0500

    updated index.js and index.html for server to interpret what a user types in as a chat message

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ io.on('connection', function(socket){
   });
 });
 
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
